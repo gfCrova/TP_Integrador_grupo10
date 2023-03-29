d7e9e5b53bc21a98dcecec92663710fc05e52083
gfCrova <giancrova.cl5@gmail.com>
2023-03-29 00:57:46 -0300

    se elimino codigo comentado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,54 +14,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*Partido partido1 = new Partido();
-        Equipo argentina = new Equipo("Argentina", "AFA");
-        Equipo brasil = new Equipo("Brasil", "CBF");
-
-        partido1.setEquipoLocal(argentina);
-        partido1.setEquipoVisitante(brasil);
-        partido1.setGolesLocal(2);
-        partido1.setGolesVisitante(1);
-
-        Pronostico pronostico1 = new Pronostico(partido1, argentina, GANADOR);
-        System.out.println(pronostico1.puntos());
-
-        System.out.println("\n"); // SEPARACIÓN **********************************************
-
-        Partido partido2 = new Partido();
-        Equipo uruguay = new Equipo("Uruguay", "AUF");
-        Equipo chile = new Equipo("Chile", "FFCh");
-
-        partido2.setEquipoLocal(uruguay);
-        partido2.setEquipoVisitante(chile);
-        partido2.setGolesLocal(2);
-        partido2.setGolesVisitante(2);
-
-        Pronostico pronostico2 = new Pronostico(partido2, chile, EMPATE);
-        System.out.println(pronostico2.puntos());
-
-        System.out.println("\n"); // SEPARACIÓN **********************************************
-
-        Partido partido3 = new Partido();
-        Equipo colombia = new Equipo("Colombia", "FCF");
-        Equipo peru = new Equipo("Perú", "FPF");
-
-        partido3.setEquipoLocal(colombia);
-        partido3.setEquipoVisitante(peru);
-        partido3.setGolesLocal(0);
-        partido3.setGolesVisitante(1);
-
-        Pronostico pronostico3 = new Pronostico(partido3, peru, PERDEDOR);
-        System.out.println(pronostico3.puntos());
-        */
-
-        //////////////////////////////////////////////////
-
         ArrayList<Partido> partidosRonda1 = new ArrayList<Partido>();
         Ronda ronda1 = new Ronda("Ronda 1", partidosRonda1);
 
         try {
-
             String resultados = "src\\main\\java\\org\\example\\csv\\resultados.csv";
 
             for (String linea : Files.readAllLines(Paths.get(resultados))) {
@@ -90,8 +46,12 @@ public class Main {
         pron1.setEquipo(ronda1.getPartidos().get(0).getEquipoLocal());
         pron1.setResultado(GANADOR);
 
-        try {
+        Pronostico pron2 = new Pronostico();
+        pron2.setPartido(ronda1.getPartidos().get(1));
+        pron2.setEquipo(ronda1.getPartidos().get(1).getEquipoVisitante());
+        pron2.setResultado(PERDEDOR);
 
+        try {
             String pronosticos = "src\\main\\java\\org\\example\\csv\\pronosticos.csv";
 
             for (String linea : Files.readAllLines(Paths.get(pronosticos))) {
@@ -104,12 +64,15 @@ public class Main {
 
                 if (gana1.contains("x")){
                     System.out.println(pron1.puntos());
+                    System.out.println(pron2.puntos());
                     break;
                 } else if (empata.contains("x")) {
                     System.out.println(pron1.puntos());
+                    System.out.println(pron2.puntos());
                     break;
                 } else if (gana2.contains("x")) {
                     System.out.println(pron1.puntos());
+                    System.out.println(pron2.puntos());
                 }
             }
         } catch (IOException e) {

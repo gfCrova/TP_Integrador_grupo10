66d3ed0c91f36ae96c60677c2772144cd7177aba
gfCrova <giancrova.cl5@gmail.com>
2023-04-05 23:01:31 -0300

    refactorizado los metodos de leer archivos y agregando comentarios de guía

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,11 +14,41 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // ***** Salida de Partidos de la Ronda
+
         ArrayList<Partido> partidosRonda1 = new ArrayList<>();
         Ronda ronda1 = new Ronda("Ronda 1", partidosRonda1);
 
+        leerResultados(resultados, partidosRonda1);
+
+        for (int i = 0; i < ronda1.getPartidos().size(); i++) {
+            System.out.print("Partido " + (i + 1) + ": " + ronda1.getPartidos().get(i));
+        }
+        System.out.println("\n");
+
+        // ***** Salida de Resultados de Pronósticos.
+
+        Persona Diego = new Persona("Diego", ronda1, new ArrayList<>());
+        Persona Julieta = new Persona("Julieta", ronda1, new ArrayList<>());
+
+        leerPronostico(pronosticos, Diego);
+        leerPronostico(pronosticos, Julieta);
+
+        // ***** Salida del Puntaje Total entre los participates.
+
+        int total1 = Diego.getRonda().totalPuntos(Diego.getPronostico(), Diego);
+        int total2 = Julieta.getRonda().totalPuntos(Julieta.getPronostico(), Julieta);
+
+        System.out.println( "\n" + "Puntaje Total: \n" +
+                Diego.getNombre() + ": " + total1 + "\n" +
+                Julieta.getNombre() + ": " + total2);
+    }
+
+    /* Función que toma un Archivo de resultados y una Lista de partidos vacía.
+        Recorre el archivo y va agregando los partidos a la Lista */
+    public static void leerResultados(String file, ArrayList<Partido> partidos) {
         try {
-            for (String linea : Files.readAllLines(Paths.get(resultados))) {
+            for (String linea : Files.readAllLines(Paths.get(file))) {
 
                 String equipo1 = linea.split(",")[0];
                 int goles1 = Integer.parseInt(linea.split(",")[1]);
@@ -29,70 +59,51 @@ public class Main {
                 Equipo visitante = new Equipo(equipo2);
 
                 Partido partido = new Partido(local, goles1, goles2, visitante);
-                partidosRonda1.add(partido);
+                partidos.add(partido);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println(ronda1.getPartidos());
-
-        //////////////////////////////////////////////////////
-
-        // Creando Pronósticos y agregándolos al ArrayList de Pronósticos de la Persona
-        Persona persona1 = new Persona("Diego", ronda1, new ArrayList<>());
-        Pronostico pron1Diego = new Pronostico(partidosRonda1.get(0), partidosRonda1.get(0).getEquipoLocal());
-        Pronostico pron2Diego  = new Pronostico(partidosRonda1.get(1), partidosRonda1.get(1).getEquipoLocal());
-        Pronostico pron3Diego  = new Pronostico(partidosRonda1.get(2), partidosRonda1.get(2).getEquipoVisitante());
-        Pronostico pron4Diego  = new Pronostico(partidosRonda1.get(3), partidosRonda1.get(3).getEquipoVisitante());
-        persona1.getPronostico().add(pron1Diego);
-        persona1.getPronostico().add(pron2Diego);
-        persona1.getPronostico().add(pron3Diego);
-        persona1.getPronostico().add(pron4Diego);
-
-        Persona persona2 = new Persona("Julieta", ronda1, new ArrayList<>());
-        Pronostico pron1Julieta = new Pronostico(partidosRonda1.get(0), partidosRonda1.get(0).getEquipoLocal());
-        Pronostico pron2Julieta  = new Pronostico(partidosRonda1.get(1), partidosRonda1.get(1).getEquipoVisitante());
-        Pronostico pron3Julieta  = new Pronostico(partidosRonda1.get(2), partidosRonda1.get(2).getEquipoLocal());
-        Pronostico pron4Julieta  = new Pronostico(partidosRonda1.get(3), partidosRonda1.get(3).getEquipoLocal());
-        persona2.getPronostico().add(pron1Julieta);
-        persona2.getPronostico().add(pron2Julieta);
-        persona2.getPronostico().add(pron3Julieta);
-        persona2.getPronostico().add(pron4Julieta);
-
-        leerPronostico(pronosticos, persona1);
-        leerPronostico(pronosticos, persona2);
-
-        int total1 = persona1.getRonda().totalPuntos(persona1.getPronostico(), persona1);
-        int total2 = persona2.getRonda().totalPuntos(persona2.getPronostico(), persona2);
-        System.out.println( "\n" + "Puntaje Total: \n" +
-                persona1.getNombre() + ": " + total1 + "\n" +
-                persona2.getNombre() + ": " + total2);
     }
 
+    /* Función que toma un Archivo de pronósticos y una Persona.
+        Se recorre la Ronda de partidos y se crea un pronóstico para cada partido.
+         Se recorre el archivo y se verifica si los resultados de los partidos coinciden con los pronósticos dados
+            por la Persona y se le asigna un Resultado */
     public static void leerPronostico(String file, Persona user) {
         try {
-            for (String linea : Files.readAllLines(Paths.get(file))) {
+            int count = 0;
+            while (count < user.getRonda().getPartidos().size()) {
 
-                String persona = linea.split(",")[0];
-                String equipo1 = linea.split(",")[1];
-                String gana1 = linea.split(",")[2];
-                String empata = linea.split(",")[3];
-                String gana2 = linea.split(",")[4];
-                String equipo2 = linea.split(",")[5];
+                Equipo equipoLocal = user.getRonda().getPartidos().get(count).getEquipoLocal();
+                Equipo equipoVisitante = user.getRonda().getPartidos().get(count).getEquipoVisitante();
 
-                for (Pronostico pron : user.getPronostico()) {
-                    String equipo = pron.getPartido().getEquipoLocal().getNombre();
+                Pronostico pr = new Pronostico(user.getRonda().getPartidos().get(count), equipoLocal);
+
+                for (String linea : Files.readAllLines(Paths.get(file))) {
+
+                    String persona = linea.split(",")[0];
+                    String equipo1 = linea.split(",")[1];
+                    String gana1 = linea.split(",")[2];
+                    String empata = linea.split(",")[3];
+                    String gana2 = linea.split(",")[4];
+                    String equipo2 = linea.split(",")[5];
+
                     if (persona.equals(user.getNombre())) {
-                        if (equipo1.equals(equipo) || equipo2.equals(equipo)) {
-                            if (gana1.contains("x") || gana2.contains("x")) {
-                                pron.setResultado(GANADOR);
+                        if (equipo1.equals(equipoLocal.getNombre()) || equipo2.equals(equipoVisitante.getNombre())) {
+                            if (gana1.contains("x")) {
+                                pr.setResultado(GANADOR);
+                            } else if (gana2.contains("x")) {
+                                pr.setEquipo(equipoVisitante);
+                                pr.setResultado(GANADOR);
                             } else if (empata.contains("x")) {
-                                pron.setResultado(EMPATE);
+                                pr.setResultado(EMPATE);
                             }
                         }
                     }
                 }
+                user.getPronostico().add(pr);
+                count++;
             }
         } catch (IOException e) {
             e.printStackTrace();

3617e0a71f0fe9e2b8063d10cb473c8b342de5df
gfCrova <giancrova.cl5@gmail.com>
2023-04-08 14:29:51 -0300

    agregando funcionalidad al test

--- src/test/java/testPuntajes.java
+++ src/test/java/testPuntajes.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class testPuntajes {
+import org.example.Main;
+import org.example.Partido;
+import org.example.Persona;
+import org.example.Ronda;
+import org.example.iFiles;
+import org.junit.jupiter.api.Assertions;
+import java.util.ArrayList;
+
+public class testPuntajes {
+    @org.junit.jupiter.api.Test
+    public void testPuntos() {
+
+        iFiles files = new iFiles();
+        ArrayList<Partido> partidosRonda1 = new ArrayList<>();
+        Ronda ronda1 = new Ronda("Ronda 1", partidosRonda1);
+        Persona Diego = new Persona("Diego", ronda1, new ArrayList<>());
+
+        Main.leerResultados(files.getFileResultados(), partidosRonda1);
+        Main.leerPronostico(files.getFilePronosticos(), Diego);
+
+        int resultadoEsperado = 3;
+        int result = Diego.getRonda().totalPuntos(Diego.getPronostico(), Diego);
+
+        Assertions.assertEquals(result, resultadoEsperado);
+    }
 }
+

1086ca69cb2d5fe80bed5c2f4dffbbe34459d369
gfCrova <giancrova.cl5@gmail.com>
2023-04-08 14:28:45 -0300

    trabajando los files. Move to resources, delete inputFiles..

---
+++ src/main/java/org/example/iFiles.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class iFiles {
+    public iFiles() {
+    }
+    public String getFileResultados() { return "src\\main\\resources\\resultados.csv";}
+
+    public String getFilePronosticos() {
+        return "src\\main\\java\\resources\\pronosticos.csv";
+    }
+}

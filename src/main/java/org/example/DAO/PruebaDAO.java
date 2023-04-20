fd39f7b49d4298d4c214231b142c8ed8eb0c29ee
gfCrova <giancrova.cl5@gmail.com>
2023-04-20 13:45:40 -0300

    Mostrando en pantalla los resultados de la consulta

--- src/main/java/org/example/DAO/PruebaDAO.java
+++ src/main/java/org/example/DAO/PruebaDAO.java
@@ -1,5 +1,12 @@
 package org.example.DAO;
 
+import org.example.Entidades.Partido;
+import org.example.Entidades.Persona;
+import org.example.Entidades.Ronda;
+import org.example.Services.FilesService;
+
+import java.util.ArrayList;
+
 public class PruebaDAO {
     public static void main(String[] args) {
 

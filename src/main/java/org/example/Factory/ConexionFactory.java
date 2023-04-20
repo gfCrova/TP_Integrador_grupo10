e6275cedb61ec2cf4ea5495dea5cc35b44b31f3c
gfCrova <giancrova.cl5@gmail.com>
2023-04-20 01:02:03 -0300

    add Factory carpeta de conexion

--- src/main/java/org/example/Factory/ConexionFactory.java
+++ src/main/java/org/example/Factory/ConexionFactory.java
@@ -1,2 +1,31 @@
-package org.example.Factory;public class ConexionFactory {
+package org.example.Factory;
+
+import org.example.Services.ConfigService;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionFactory {
+
+    public static Connection getConnection() {
+
+        ConfigService load = new ConfigService();
+        load.obtenerCofiguracion();
+
+        String url = load.getUrl();
+        String username = load.getUsername();
+        String password = load.getPassword();
+
+        String driverName = load.getDriver();
+
+        Connection con = null;
+        try {
+            Class.forName(driverName);
+            con = DriverManager.getConnection(url,username,password);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
 }

dd460181fc30157d3722d3e6bd55a5c7f78cd904
gfCrova <giancrova.cl5@gmail.com>
2023-04-24 01:20:49 -0300

    trabajando en salida de pronósticos con puntos extra

--- src/main/java/org/example/Services/PronosticosService.java
+++ src/main/java/org/example/Services/PronosticosService.java
@@ -6,42 +6,39 @@ import org.example.Entidades.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-import static org.example.Enums.ResultadoEnum.EMPATE;
-import static org.example.Enums.ResultadoEnum.GANADOR;
+import static org.example.Enums.ResultadoEnum.*;
 
 public class PronosticosService {
 
+    PronosticosDAO dao = new PronosticosDAO();
+    List<Pronostico> listaPron = dao.listarPronosticos();
+    List<Persona> listaPersonas = dao.listarPersonas();
+
+    PartidosDAO partidosDAO = new PartidosDAO();
+    List<Partido> listaPartidos = partidosDAO.obtenerPartidos();
+    List<Ronda> listaRondas =  partidosDAO.obtenerRondas();
+
+    ConfigService configService = new ConfigService();
+
+    Pronostico pr;
+
     public void generarPronosticos() {
-
-        PronosticosDAO dao = new PronosticosDAO();
-        List<Pronostico> listaPron = dao.listarPronosticos();
-        List<Persona> listaPersonas = dao.listarPersonas();
-
-        PartidosDAO partidosDAO = new PartidosDAO();
-        List<Partido> listaPartidos = partidosDAO.obtenerPartidos();
-        List<Ronda> listaRondas =  partidosDAO.obtenerRondas();
-
-        for (Ronda ronda : listaRondas) {
-            for (Partido partido : listaPartidos) {
-                if (partido.getRondaNumero() == ronda.getId()) {
-                    ronda.setPartidos(listaPartidos);
-                }
-            }
-        }
 
         for (Persona persona : listaPersonas) {
             int count = 0;
             persona.setRonda(listaRondas.get(count));
-            persona.setPronostico(new ArrayList<Pronostico>());
+            persona.setPronostico(new ArrayList<>());
             while (count < persona.getRonda().getPartidos().size()) {
 
                 Equipo equipoLocal = persona.getRonda().getPartidos().get(count).getEquipoLocal();
                 Equipo equipoVisitante = persona.getRonda().getPartidos().get(count).getEquipoVisitante();
 
-                Pronostico pr = new Pronostico(persona.getRonda().getPartidos().get(count), equipoLocal);
+                pr = new Pronostico(persona.getRonda().getPartidos().get(count), equipoLocal);
 
                 for (Pronostico lpr : listaPron) {
+                    pr.setIdPersona(lpr.getIdPersona());
                     if (lpr.getPersona().equals(persona.getNombre())) {
                         if (lpr.getEquipo1().equals(equipoLocal.getNombre()) || lpr.getEquipo2().equals(equipoVisitante.getNombre())) {
                             if (lpr.getGana1().contains("x")) {
@@ -55,34 +52,37 @@ public class PronosticosService {
                         }
                     }
                 }
+                salidaPronosticos(persona, count);
+                configService.obtenerCofiguracion();
                 persona.getPronostico().add(pr);
-
-                if (pr.puntos(persona) == 1) {
-                    System.out.println(persona.getNombre() + " - Pronóstico Partido " + (count + 1) + ": ACERTADO!. Puntos: +" + pr.puntos(persona));
-                } else {
-                    System.out.println(persona.getNombre() + " - Pronóstico Partido " + (count + 1) + ": NO acertado. Puntos: " + pr.puntos(persona));
-                }
                 count++;
             }
-
-            generarPuntaje(persona);
+            System.out.println("\n");
+        }
+        for (Persona per : listaPersonas) {
+            total(per);
         }
     }
 
-    public void generarPuntaje(Persona persona) {
-        // Total de Puntos de partidos acertados
+    public void total(Persona persona) {
         int total = persona.getRonda().totalPuntos(persona.getPronostico(), persona);
-        System.out.println("Puntaje Total de " + persona.getNombre() +  " es = " + total + "\n");
+        System.out.println(persona.getNombre() + " - Puntaje Total: " + total);
+    }
 
-        // Puntos Extra por 'Acertar' todos los pronósticos de la Ronda
-        ConfigService configService = new ConfigService();
-        configService.obtenerCofiguracion();
-        // total == 5
-        if (total == persona.getRonda().getPartidos().size()) {
-            total += configService.getPuntosExtraRonda();
-            System.out.println(persona.getNombre() + " ACERTASTE TODOS LOS PRONÓSTICOS DE LA RONDA!");
-            System.out.println("Bofificación de Puntos: +" + configService.getPuntosExtraRonda());
-            System.out.println("Puntaje Total de " + persona.getNombre() + " es = " + total + "\n");
+    public void salidaPronosticos(Persona persona, int count) {
+
+        if (listaRondas.get(count).getId() <= 7) {
+            if (pr.puntos(persona) == configService.getPuntosPronAcertado()) {
+                pr.setComprobar(ACERTADO);
+                System.out.println(persona.getNombre() + " - Pronóstico Ronda: " + pr.getPartido().getRondaNumero() + " - " + listaRondas.get(count).getNombre() + " Partido: " + pr.getPartido().getId() + " " + pr.getComprobar() + "!. Puntos: +" + pr.puntos(persona));
+                if (Objects.equals(listaRondas.get(count).getId(), pr.getPartido().getRondaNumero()) && Objects.equals(pr.getComprobar(), ACERTADO)) {
+                    System.out.println(persona.getNombre() + " ACERTASTE TODOS LOS PRONÓSTICOS DE LA RONDA " + pr.getPartido().getRondaNumero());
+                    System.out.println("Bofificación de Puntos: +" + configService.getPuntosExtraRonda());
+                }
+            } else {
+                pr.setComprobar(ERRADO);
+                System.out.println(persona.getNombre() + " - Pronóstico Ronda: " + pr.getPartido().getRondaNumero() + " - " + listaRondas.get(count).getNombre() + " Partido: " + pr.getPartido().getId() + " " + pr.getComprobar() + "!. Puntos: " + pr.puntos(persona));
+            }
         }
     }
 }
